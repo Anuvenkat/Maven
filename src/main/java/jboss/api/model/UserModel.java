7465dbdd9e12959512f7ace9f31528f8a78fa4f5
devops <venkatesh.kandanuri@gmail.com>
2018-03-04 12:44:15 +0530

    Maven proj

---
+++ src/main/java/jboss/api/model/UserModel.java
@@ -0,0 +1,44 @@
+package jboss.api.model;
+
+import jboss.ejb.User;
+
+public class UserModel {
+
+    private String username, firstName, lastName;
+
+    public UserModel() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public static UserModel parse(User user) {
+        UserModel userModel = new UserModel();
+        userModel.setUsername(user.getUsername());
+        userModel.setFirstName(user.getFirstName());
+        userModel.setLastName(user.getLastName());
+
+        return userModel;
+    }
+}
